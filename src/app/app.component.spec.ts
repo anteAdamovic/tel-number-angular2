84c1d07fd6647b15c49015de9cd7a2141739c7c1
anteAdamovic <ante.adamovic93@gmail.com>
2023-02-01 15:54:12 +0300

    Initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'tel-number'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tel-number');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { MatOptionSelectionChange } from '@angular/material/core';
+
+interface CountryCode {
+  id: number;
+  code: string;
+  img: string;
+}
 
 @Component({
   selector: 'm-root',
@@ -6,5 +13,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tel-number';
+  public countryCodes: CountryCode[] = [
+    {
+      id: 1,
+      code: "+385",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/2560px-Flag_of_Croatia.svg.png"
+    },
+    {
+      id: 2,
+      code: "+47",
+      img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Norway.svg/468px-Flag_of_Norway.svg.png"
+    }
+  ];
+
+  @ViewChild("numberInput") numberInput!: ElementRef<HTMLInputElement>;
+
+  public countryCodeSelected(event: MatOptionSelectionChange) {
+    setTimeout(() => this.numberInput.nativeElement.focus(), 10);
+  }
 }
